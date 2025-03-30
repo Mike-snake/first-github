49f488549e1859203d7fbb8867245102fea2fc46
Mike-snake <michaelmw@naver.com>
2025-03-31 01:05:29 +0900

    first commit
    GUGUDan

---
+++ Ex02_구구단.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+int gugudan(int a);
+
+int main()
+{
+
+	gugudan(9);
+
+	return 0;
+}
+
+int gugudan(int a)
+{
+	if (a <= 0)
+		return;
+
+
+	//printf("2 X %d = %d\n", a, 2 * a);
+	gugudan(a - 1);
+	printf("2 X %d = %d\n", a, 2 * a);
+
+	return a;
+
+
+
+
+}
+
+
+
+
+
+
+
+
